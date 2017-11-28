34ff43f37fb691791341789531a2faa62bc3d00a
BruceBayne <bruce-bayne@hotmail.com>
2017-11-28 05:46:11 +0200

    + EventUtils class provide API making environment (Type to Event call)

---
+++ Friday.Base/Reflection/EventUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace Friday.Base.Reflection
+{
+	public static class EventUtils
+	{
+		public const int EventArgsArgCount = 2;
+		public const int EventArgsObjectParameter = 1;
+
+		/// <summary>
+		/// Search for EventHandler with type of 'eventArgument' in 'target' and Invoke that event 
+		/// </summary>
+		/// <param name="target">Target with EventHandler</param>
+		/// <param name="eventArgument">Invoked Event Argument</param>
+		public static void InvokeEvent(object target, object eventArgument)
+		{
+			if (eventArgument == null)
+				throw new ArgumentNullException(nameof(eventArgument));
+
+			var fields = target.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+			foreach (var field in fields)
+			{
+				var eventDelegate = field.GetValue(target) as Delegate;
+
+
+
+				if (eventDelegate?.GetMethodInfo().GetParameters().Length != EventArgsArgCount)
+					continue;
+
+
+				var eventArgType = eventDelegate.GetMethodInfo().GetParameters()[EventArgsObjectParameter].ParameterType;
+
+				if (eventArgType != eventArgument.GetType())
+					continue;
+
+				foreach (var eventHandler in eventDelegate.GetInvocationList())
+					eventHandler.Method.Invoke(eventHandler.Target, new[] { target, eventArgument });
+			}
+		}
+	}
+}
---
+++ Tests/Friday.Base.Tests/Reflection/EventUtilsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using Friday.Base.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Friday.Base.Tests.Reflection
+{
+	[TestClass]
+	public class EventUtilsTests
+	{
+		private class ClassWithEvents
+		{
+			public event EventHandler<string> OnString;
+			public event EventHandler<int> OnInt;
+		}
+
+		[TestMethod]
+		public void EventInvocationInThirdPartyClassShouldSuccess()
+		{
+
+
+			var e = new ClassWithEvents();
+			var counter = 0;
+			e.OnString += (sender, s) => counter++;
+			e.OnString += (sender, s) => counter++;
+
+			EventUtils.InvokeEvent(e, string.Empty);
+
+			//Unfortunately FluentAssertions MonitorEvent/ShouldRaise Methods make this test fail
+			counter.Should().Be(2);
+		}
+	}
+}
